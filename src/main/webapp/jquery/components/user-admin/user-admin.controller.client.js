578b7102f970a2b847ab0b44f5c91e9df84ae9e0
parmeetsingh <saluja.parmeetsingh@gmail.com>
2018-05-11 14:12:07 -0500

    Added functionality of reading data from users

--- src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
+++ src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
@@ -1,8 +1,56 @@
-(function(){
-	jQuery(main);
-	function main(){
-		var tr = $('.template');
-		var tbody = $('tbody');
-		tbody.append()
-	}
+(function () {
+
+    jQuery(main);
+
+    var tbody;
+    var template;
+
+    function main() {
+        tbody = $('tbody');
+        template = $('.template');
+        $('#createUser').click(createUser);
+
+        var promise = fetch('http://localhost:8080/api/user');
+        promise.then(function (response) {
+            return response.json();
+        }).then(renderUsers)
+    }
+
+    function createUser() {
+        console.log('createUser');
+
+        var username = $('#usernameFld').val();
+        var password = $('#passwordFld').val();
+        var firstName = $('#firstNameFld').val();
+        var lastName = $('#lastNameFld').val();
+
+        var user = {
+            username: username,
+            password: password,
+            firstName: firstName,
+            lastName: lastName
+        };
+
+        fetch('http://localhost:8080/api/user', {
+            method: 'post',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
+    }
+
+    function renderUsers(users) {
+        for(var i=0; i<users.length; i++) {
+            var user = users[i];
+            var clone = template.clone();
+            clone.find('.username').html(user.username);
+            clone.find('.password').html(user.password);
+            clone.find('.firstName').html(user.firstName);
+            clone.find('.lastName').html(user.lastName);
+            clone.find('.date').html(user.date);
+            tbody.append(clone);
+        }
+    }
+
 })();
