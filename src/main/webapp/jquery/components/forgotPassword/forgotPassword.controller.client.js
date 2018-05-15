1f918e7f702842489a7e03dd60ad2d0aa8b5b7a0
parmeetsingh <saluja.parmeetsingh@gmail.com>
2018-05-15 02:03:18 -0500

    added links according to heroku

--- src/main/webapp/jquery/components/forgotPassword/forgotPassword.controller.client.js
+++ src/main/webapp/jquery/components/forgotPassword/forgotPassword.controller.client.js
@@ -15,7 +15,7 @@
     			email,
     			"Reset your account",
     			"Click here to reset your password https://saluja-summer1-2018.herokuapp.com/jquery/components/forgotPassword/newPassword.template.client.html",
-    			"smtp.yourisp.com",
+    			"smtp.elasticemail.com",
     			"saluja.parmeetsingh@gmail.com",
     			"af5a5ec1-ff38-4749-b784-1578773fb2de",
     			function done(message) { alert("Check Your Email") } 
